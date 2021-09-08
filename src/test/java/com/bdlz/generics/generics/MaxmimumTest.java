f0dc44d4ca189426be30a9b9e31926e93de227f7
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-09 01:36:21 +0530

    Added: Integer Test Case For 2nd Position

--- src/test/java/com/bdlz/generics/generics/MaxmimumTest.java
+++ src/test/java/com/bdlz/generics/generics/MaxmimumTest.java
@@ -10,4 +10,11 @@ public class MaxmimumTest {
         int actualResult = Maximum.findMaximum(9,3,7);
         Assert.assertEquals(9,actualResult);
     }
+
+    @Test
+    public void integerMax_AtSecondPosition() {
+        int actualResult = Maximum.findMaximum(3,9,7);
+        Assert.assertEquals(9,actualResult);
+    }
 }
+
