d9273f2478718014b0dedf3ab20267003ed10426
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-09 04:37:08 +0530

    Addded: Printing Max

--- src/test/java/com/bdlz/generics/generics/MaxmimumTest.java
+++ src/test/java/com/bdlz/generics/generics/MaxmimumTest.java
@@ -11,6 +11,7 @@ public class MaxmimumTest {
         Integer[] inputarray = { 1, 2, 3, 4, 5 };
         int finalResult = Maximum.findMaximum(inputarray);
         Assert.assertEquals(5, finalResult);
+        System.out.println(finalResult);
     }
 
     @Test
@@ -18,6 +19,7 @@ public class MaxmimumTest {
         Integer[] inputarray = { 63, 71, 84, 96 ,59};
         int finalResult = Maximum.findMaximum(inputarray);
         Assert.assertEquals(96, finalResult);
+        System.out.println(finalResult);
     }
 
     @Test
@@ -25,6 +27,7 @@ public class MaxmimumTest {
         Integer[] inputarray = { 876, 394, 675, 942, 239, 596 };
         int finalResult = Maximum.findMaximum(inputarray);
         Assert.assertEquals(942, finalResult);
+        System.out.println(finalResult);
     }
 
     @Test
@@ -32,6 +35,7 @@ public class MaxmimumTest {
         Float[] inputarray = { 2.9f, 9.1f, 8.3f, 4.6f, 3.7f};
         float finalResult = Maximum.findMaximum(inputarray);
         Assert.assertEquals(9.1f, finalResult, 0.0);
+        System.out.println(finalResult);
 
     }
 
@@ -40,6 +44,7 @@ public class MaxmimumTest {
         Float[] inputarray = { 10.12f, 24.56f, 53.2f, 67.2f, 55.7f };
         float finalResult = Maximum.findMaximum(inputarray);
         Assert.assertEquals(67.2f, finalResult, 0.0);
+        System.out.println(finalResult);
     }
 
     @Test
@@ -47,6 +52,7 @@ public class MaxmimumTest {
         Float[] inputarray = { 369.4f, 473.5f, 161.9f, 892.4f, 781.6f };
         float finalResult = Maximum.findMaximum(inputarray);
         Assert.assertEquals(892.4f, finalResult, 0.0);
+        System.out.println(finalResult);
     }
 
     @Test
@@ -54,6 +60,7 @@ public class MaxmimumTest {
         String[] inputarray = { "Peach", "Apple", "Banana", "Mango", "Grape" };
         String finalResult = Maximum.findMaximum(inputarray);
         Assert.assertEquals("Peach", finalResult);
+        System.out.println(finalResult);
 
     }
 
@@ -62,6 +69,7 @@ public class MaxmimumTest {
         String[] inputarray = { "Grape", "Peach", "Mango", "Apple", "Banana" };
         String finalResult = Maximum.findMaximum(inputarray);
         Assert.assertEquals("Peach", finalResult);
+        System.out.println(finalResult);
     }
 
     @Test
@@ -69,6 +77,7 @@ public class MaxmimumTest {
         String[] inputarray = { "Mango", "Peach", "Apple", "Grape", "Banana" };
         String finalResult = Maximum.findMaximum(inputarray);
         Assert.assertEquals("Peach", finalResult);
+        System.out.println(finalResult);
     }
 }
 
